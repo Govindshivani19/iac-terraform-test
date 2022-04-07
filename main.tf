7c1020c0b3ade1688a33f7acde0ce25192275331
Govindshivani19 <68057087+Govindshivani19@users.noreply.github.com>
2022-04-07 09:43:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
-
 resource "aws_s3_bucket" "b" {
   bucket = "my-terraform-iac-test-bucket"
 
